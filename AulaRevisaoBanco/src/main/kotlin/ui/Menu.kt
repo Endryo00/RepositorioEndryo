da91754793773a6c7526a079162d5fb62d78bb6e
Endryo00 <moraesendryo@hotmail.com>
2025-08-27 20:07:39 -0300

    Update Menu.kt

--- AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
+++ AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
@@ -6,6 +6,15 @@ fun menu()
     println("2 - Editar Caixa D'Agua ")
     println("3 - Listar Caixa D'Agua ")
     println("4 - Excluir Caixa D'Agua ")
-    println("5 - Cadastrar Caixa D'Agua ")
+
+    var opcao = readln().toInt()
+
+    when (opcao) {
+        1 -> println("Cadastrando caixa...")
+        2 -> println("Editando caixa...")
+        3 -> println("Listando caixa...")
+        4 -> println("Excluindo caixa...")
+        else -> println("Opçao Invalida, por favor digite o numero da opção")
+    }
 
 }
